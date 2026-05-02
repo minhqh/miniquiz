a02b3c894888c9a994d7e90dfb77d13972efe271
minhqh <minh.hoquang.dev@gmail.com>
2026-05-02 16:39:43 +0700

    Thêm kết nối tới api gemini

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
+
+class Request(BaseModel):
+    topic: str
 
 @app.get("/")
 def get_root():
     return {"message" : "Xin chào Milynx"}
 
+@app.post("/generate")
+def generate_mcq(req: Request):
+    try:
+        prompt = f"""
+        Generate 1 multiple choice question about: {req.topic}
+
+        Return JSON format:
+        {{
+            "question": "...",
+            "options": ["A", "B", "C", "D"],
+            "answer": "A"
+        }}
+        """
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
+
+        return {"result": response.text}
+    except Exception as e:
+        return {"error": str(e)}
